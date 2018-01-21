abe2563c58ac41d3fe13b33909c74956701b4d07
cesarbess <cesar.bess@gmail.com>
2018-01-21 21:24:34 -0200

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+  http_basic_authenticate_with name: "cesar", password: "123", except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # render plain: params[:post].inspect
+    @post = Post.new(post_params)
+
+    #need to call the save method for it to actually save
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  #Defines that the Post model will only permit a 'title' and a 'body' in its params
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root route ~ 'controller name#view name'
+  root 'posts#index', as: 'home'
+
+  #Regular route
+  get 'about' => 'pages#about', as: 'about'
+
+  #Resource
+  resources :posts do
+    resources :comments
+  end
+
+  # Generates the paths
+  # posts_path
+  # post_path
+  # new_post_path
+  # edit_post_path
+
+end
